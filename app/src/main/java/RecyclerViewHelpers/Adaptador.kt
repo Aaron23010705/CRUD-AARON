811b365bf34c10dca1952b2589e6fc5970555e17
20230406@ricaldone.edu.sv <20200636@ricaldone.edu.sv>
2024-05-20 11:03:14 -0600

    Agregue imagenes en el card para editar y borrar

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -11,21 +11,25 @@ import kotlinx.coroutines.withContext
 import modelo.ClaseConexion
 import modelo.dataClassMascotas
 
-
+//El adaptador es la clase que me ayuda a mostrar los datos,
 class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<ViewHolder>() {
 
     fun ActualizarLista(nuevoLista: List <dataClassMascotas>){
         Datos = nuevoLista
         notifyDataSetChanged()//Esto mnotifica al RecyclerView que hay datos nuevos
     }
+
+    //on createviewholder es el metodo que nos permite enlazar la carta creada con nuestro recycle view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista =
             LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
 
         return ViewHolder(vista)
     }
-    override fun getItemCount() = Datos.size
 
+    //Devolver los datos existentes, si tenemos 5 mascotas, devolveria 5 mascotas
+    override fun getItemCount() = Datos.size
+//Nos ayuda a controlar los clicks que el usuario hace en las cartas, es decir permite al usuario interactuar con la carta
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val producto = Datos[position]
         holder.textView.text = producto.nombreMascota
--- app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
+++ app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
@@ -8,4 +8,8 @@ import aaron.garcia.crud.R
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val textView: TextView = view.findViewById(R.id.txtMascotaCard)
+    val imgEditar: ImageView = view.findViewById(R.id.imgEditar)
+    val imgBorrar: ImageView = view.findViewById(R.id.imgBorrar)
+
+
 }

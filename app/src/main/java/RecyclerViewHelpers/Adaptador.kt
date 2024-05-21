11fb47c41cf21ab88d3cb15214bb01c038e23ce2
20230406@ricaldone.edu.sv <20200636@ricaldone.edu.sv>
2024-05-21 15:22:55 -0600

    se agrego eliminar del crud

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -1,11 +1,13 @@
 package RecyclerViewHelper
 
 import aaron.garcia.crud.R
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import modelo.ClaseConexion
@@ -19,6 +21,33 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
         notifyDataSetChanged()//Esto mnotifica al RecyclerView que hay datos nuevos
     }
 
+    ////////////////// TODO: Eliminar datos
+
+    //Lo que pasa aq
+    fun eliminarDatos (nombreMascotas:String, posicion: Int) {
+        //Actualizo la lista de datos y notifico al adaptador
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(posicion)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            // creamos un objeto de la clase conexion
+
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            // 2- Crear una variable que contenga un preparestatement (donde se mete el código de sqlserver
+            val deleteMascota = objConexion?.prepareStatement( "delete from tbMascotas where nombreMascota = ?")!!
+            deleteMascota.setString(1, nombreMascotas)
+            deleteMascota.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")!!
+            commit.executeUpdate()
+        }
+         Datos = listaDatos.toList()
+        //Notificar el adaptador sobre los cambios
+        notifyItemRemoved(posicion)
+        notifyDataSetChanged()
+    }
+
     //on createviewholder es el metodo que nos permite enlazar la carta creada con nuestro recycle view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista =
@@ -27,12 +56,58 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
         return ViewHolder(vista)
     }
 
-    //Devolver los datos existentes, si tenemos 5 mascotas, devolveria 5 mascotas
+    //Devolver los datos existentes, si tenemos 5 mascotas, devolveria 5 mascotas     b
     override fun getItemCount() = Datos.size
+
+
 //Nos ayuda a controlar los clicks que el usuario hace en las cartas, es decir permite al usuario interactuar con la carta
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val producto = Datos[position]
-        holder.textView.text = producto.nombreMascota
+    val mascota = Datos[position]
+    holder.textView.text = mascota.nombreMascota
+
+
+holder.imgEditar.setOnClickListener {
+    val context = holder.itemView.context
+
+    val builder = AlertDialog.Builder(context)
+    builder.setTitle("Editar")
+    builder.setMessage("Estas seguro que quieres editar?")
+
+
+    builder.setPositiveButton("Si") { dialog, which ->
+        dialog.dismiss()
+    }
+    builder.setNegativeButton("no") { dialog, which ->
+        dialog.dismiss()
     }
 
+    val dialog = builder.create()
+    dialog.show()
 }
+    //todo: click al icono de eliminar
+    holder.imgBorrar.setOnClickListener {
+
+        val context = holder.itemView.context
+
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Elimnar")
+        builder.setMessage("¿Está seguro que quiere eliminar la mascota?")
+
+        //Botones
+        builder.setPositiveButton("Si") { dialog, which ->
+            eliminarDatos(mascota.nombreMascota, position)
+        }
+            builder.setNegativeButton("no") { dialog, which ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+
+
+        }
+
+    }
+    }
+
+
